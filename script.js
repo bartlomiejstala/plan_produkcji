f2a7b67936ec3e561df5121682ba6afe51b3e615
bartlomiejstala <bartlomiej.pytko@stalatube.com>
2026-07-08 20:43:13 +0200

    Update script.js

--- script.js
+++ script.js
@@ -189,8 +189,7 @@ function drawSheet(index){
         // ===============================
 
 
-        if(
-            if (row[statusIndex] && row[statusIndex].toLowerCase().includes("production")) {
+        if (row[statusIndex] && row[statusIndex].toLowerCase().includes("production")) {
     console.log("PRODUCTION ZNALEZIONE", row);
     tr.classList.add("productionRow");
 }
